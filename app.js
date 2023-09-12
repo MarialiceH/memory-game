9700735d9dade286e4105c6316bea9a4f56e2695
MarialiceH <137061846+MarialiceH@users.noreply.github.com>
2023-09-12 10:51:40 -0400

    updated JavaScript

--- app.js
+++ app.js
@@ -1,6 +1,57 @@
 const cardArray = [
     {
         name: 'fries',
-        img: '/images/fries.png',
+        img: 'images/fries.png',
     },
+    {
+        name: 'cheesburger',
+        img: 'images/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    },
+    {
+        name: 'icecream',
+        img: 'images/icecream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'cheesburger',
+        img: 'images/cheeseburger.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    },
+    {
+        name: 'icecream',
+        img: 'images/icecream.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    }
 ]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard ()
